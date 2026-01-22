2ae62fe3c749445d03f5131ddec534a68a8c9672
Alexander Davis <alexlovesmolecules@gmail.com>
2026-01-21 22:29:06 -0500

    Added an energy task to the pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,8 @@
 from read_ligand_smiles_file import ReadLigandSmilesTask
 from ligate_homoleptic_iridium import LigateHomolepticIrTask
 from molblock_to_octahedral_geometries import MolblockToOctahedralGeometriesTask
+from make_energy_task import make_energy_task
+from setup_functions import tblite_singlet_setup, tblite_triplet_setup
 from dplutils.pipeline import PipelineGraph
 from dplutils.cli import get_argparser, cli_run
 from dplutils.pipeline.ray import RayStreamGraphExecutor
@@ -17,6 +19,8 @@ graph = PipelineGraph([
     ReadLigandSmilesTask,
     LigateHomolepticIrTask,
     MolblockToOctahedralGeometriesTask,
+    make_energy_task(tblite_singlet_setup, 'octahedral_embed_xyz', 'tblite_singlet_energy'),
+    make_energy_task(tblite_triplet_setup, 'octahedral_embed_xyz', 'tblite_triplet_energy')
     ])
 
 executor = RayStreamGraphExecutor(graph, max_batches=1)
