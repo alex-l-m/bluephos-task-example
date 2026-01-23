df341826abd5c9a1fe0d593a7b432a380825d216
Alexander Davis <alexlovesmolecules@gmail.com>
2026-01-22 19:20:20 -0500

    Remove source task, read table into a generator

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,6 @@
-from read_ligand_smiles_file import ReadLigandSmilesTask
+import pandas as pd
+import ray
+
 from ligate_homoleptic_iridium import LigateHomolepticIrTask
 from molblock_to_octahedral_geometries import MolblockToOctahedralGeometriesTask
 from make_ase_tasks import make_energy_task, make_optimization_task
@@ -7,8 +9,6 @@ from dplutils.pipeline import PipelineGraph
 from dplutils.cli import get_argparser, cli_run
 from dplutils.pipeline.ray import RayStreamGraphExecutor
 
-import ray
-
 ap = get_argparser()
 ap.set_defaults(file='run.yaml')
 args = ap.parse_args()
@@ -16,7 +16,6 @@ args = ap.parse_args()
 ray.init()
 
 graph = PipelineGraph([
-    ReadLigandSmilesTask,
     LigateHomolepticIrTask,
     MolblockToOctahedralGeometriesTask,
     make_optimization_task(tblite_triplet_setup, 'octahedral_embed_xyz', 'tblite_triplet_optimized_xyz'),
@@ -24,6 +23,8 @@ graph = PipelineGraph([
     make_energy_task(tblite_triplet_setup, 'tblite_triplet_optimized_xyz', 'tblite_triplet_energy')
     ])
 
-executor = RayStreamGraphExecutor(graph, max_batches=1)
+executor = RayStreamGraphExecutor(graph,
+        generator=lambda: pd.read_csv("in.csv", chunksize=200),
+)
 
 cli_run(executor, args)
